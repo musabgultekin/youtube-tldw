62b3798283fb240a4f0cac8d33b15ca1bc87bb4c
Musab Gültekin <gultekinmusab@gmail.com>
2023-04-29 22:38:57 +0300

    Add basic script

---
+++ main.py
@@ -0,0 +1,14 @@
+import textwrap
+import openai
+from youtube_transcript_api import YouTubeTranscriptApi
+from urllib.parse import parse_qs, urlparse
+
+openai.api_key = "" # @param {type:"string"}
+url = "" # @param {type:"string"}
+transcript = YouTubeTranscriptApi.get_transcript(parse_qs(urlparse(url).query).get('v')[0])
+
+for i in range(0, len(transcript), 1000):
+    context = "\n".join([t["text"] for t in transcript[i:i+1000]])
+    prompt = f"Summarize this youtube video section:\n---\n{context}" # @param {type: "string"}
+    summary = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}]).choices[0].message["content"]
+    print(textwrap.fill(summary, 100))
