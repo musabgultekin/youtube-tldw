98e5c92fa9ab07a29cbe2ebfacaef7f1070db9b3
Musab Gültekin <gultekinmusab@gmail.com>
2023-04-29 23:06:05 +0300

    Handle errors

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ import openai
 from youtube_transcript_api import YouTubeTranscriptApi
 
 def summarize_video(url):
-    transcript = YouTubeTranscriptApi.get_transcript(parse_qs(urlparse(url).query).get('v')[0])
+    try:
+        video_id = parse_qs(urlparse(url).query).get('v')[0]
+    except IndexError:
+        print("Invalid YouTube video URL.")
+        sys.exit(1)
+
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    except Exception as e:
+        print(f"Error fetching transcript: {e}")
+        sys.exit(1)
 
     for i in range(0, len(transcript), 1000):
         context = "\n".join([t["text"] for t in transcript[i:i+1000]])
-        prompt = f"Summarize this youtube video section:\n---\n{context}" # @param {type: "string"}
-        summary = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}]).choices[0].message["content"]
+        prompt = f"Summarize this youtube video section:\n---\n{context}"
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}]
+        )
+        summary = response.choices[0].message["content"]
         print(summary)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Summarize YouTube video transcripts.")
